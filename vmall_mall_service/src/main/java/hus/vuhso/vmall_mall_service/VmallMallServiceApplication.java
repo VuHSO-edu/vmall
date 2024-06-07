1d655d0672a8da82df29a8241ab83d8c4eccc6b6
nekkoICU <nv.vuhso@gmail.com>
2024-06-07 20:03:53 +0700

    vmall_service

---
+++ vmall_mall_service/src/main/java/hus/vuhso/vmall_mall_service/VmallMallServiceApplication.java
@@ -0,0 +1,13 @@
+package hus.vuhso.vmall_mall_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallMallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmallMallServiceApplication.class, args);
+    }
+
+}
---
+++ vmall_shipment_service/src/main/java/hus/vuhso/vmall_shipment_service/VmallShipmentServiceApplication.java
@@ -0,0 +1,13 @@
+package hus.vuhso.vmall_shipment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallShipmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmallShipmentServiceApplication.class, args);
+    }
+
+}
