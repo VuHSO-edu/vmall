fdb4bc07b55d0f029f5c5940e8fb6775fe5f14f7
nekkoICU <nv.vuhso@gmail.com>
2024-06-07 20:19:08 +0700

    vmall_service

---
+++ vmall_user_manager/src/test/java/hus/vuhso/vmall_user_manager/VmallUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hus.vuhso.vmall_user_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallUserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
